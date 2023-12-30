188cd982d4707698ee31d49466b99e215679b216
Asish <66243448+assh@users.noreply.github.com>
2023-12-31 00:12:31 +0400

    Add pygame initialization and window setup

---
+++ app.py
@@ -0,0 +1,12 @@
+# import pygame
+import pygame
+import sys
+
+pygame.init()
+
+WIDTH, HEIGHT = 600, 600
+ROWS, COLS = 3, 3
+SQUARE_SIZE = WIDTH//COLS
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
