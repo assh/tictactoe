040bcdef3cfe185b976e73a404c7554485026dbc
Asish <66243448+assh@users.noreply.github.com>
2023-12-31 00:18:57 +0400

    Add mouse input functionality to the Tic Tac Toe game

--- app.py
+++ app.py
@@ -13,3 +13,22 @@ pygame.display.set_caption("Tic Tac Toe")
 
 board = [[-1 for _ in range(COLS)] for _ in range(ROWS)]
 
+run = True
+current_player = 1
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            col = x // SQUARE_SIZE
+            row = y // SQUARE_SIZE
+            if board[row][col] == -1:
+                if current_player == 1:
+                    board[row][col] = 'O'                   
+                else:
+                    board[row][col] = 'X'
+                current_player = current_player * -1
+
+pygame.quit()
