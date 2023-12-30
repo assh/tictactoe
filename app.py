c617fee77f9199cf5287590b704ba73193a9b8d9
Asish <66243448+assh@users.noreply.github.com>
2023-12-31 00:13:25 +0400

    Add game title and initialize game board

--- app.py
+++ app.py
@@ -9,4 +9,7 @@ ROWS, COLS = 3, 3
 SQUARE_SIZE = WIDTH//COLS
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tic Tac Toe")
+
+board = [[-1 for _ in range(COLS)] for _ in range(ROWS)]
 
