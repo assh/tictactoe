859549b9fef851a13781a7a0d03ff893ef2c3837
Asish <66243448+assh@users.noreply.github.com>
2023-12-31 00:46:29 +0400

    Add win condition checks and handle tie game

--- app.py
+++ app.py
@@ -28,18 +28,26 @@ def draw_grid():
 
 
 def game_condition():
+    # Check rows
     for row in range(ROWS):
         if board[row][0] != -1 and board[row][0] == board[row][1] == board[row][2]:
             return board[row][0]
     
+    # Check columns
     for col in range(COLS):
         if board[0][col] != -1 and board[0][col] == board[1][col] == board[2][col]:
             return board[0][col]
     
+    # Check main diagonal
     if board[0][0] != -1 and board[0][0] == board[1][1] == board[2][2]:
         return board[0][0]
     
-    count = sum(sublist.count(-1) for sublist in board)
+    # Check secondary diagonal
+    if board[0][2] != -1 and board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+    
+    # Check for tie
+    count = sum(row.count(-1) for row in board)
     if count == 0:
         return -2
     
@@ -103,5 +111,6 @@ while run:
         draw_popup("It's a tie!")
         pygame.display.update()
         pygame.time.wait(3000)
+        run = False
 
 pygame.quit()
