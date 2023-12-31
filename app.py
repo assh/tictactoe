bbc5e6845d969d82ffdbb8597c4931623d81cbe4
Asish <66243448+assh@users.noreply.github.com>
2023-12-31 08:14:05 +0400

    Removed modules to other files

--- app.py
+++ app.py
@@ -1,80 +1,17 @@
-# import pygame
 import pygame
-
-pygame.init()
-
-WIDTH, HEIGHT = 600, 600
-ROWS, COLS = 3, 3
-SQUARE_SIZE = WIDTH//COLS
-
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-WIN.fill((255, 255, 255))
-
-pygame.display.set_caption("Tic Tac Toe")
-
-board = [[-1 for _ in range(COLS)] for _ in range(ROWS)]
-
-def draw_grid():
-    for row in range(ROWS):
-        for col in range(COLS):
-            pygame.draw.rect(WIN, (0, 0, 0), (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 1)
-            if board[row][col] != -1:
-                if board[row][col] == 'O':
-                    pygame.draw.circle(WIN, (0, 0, 0), (col * SQUARE_SIZE + SQUARE_SIZE//2, row * SQUARE_SIZE + SQUARE_SIZE//2), SQUARE_SIZE//2 - 10, 5)
-                else:
-                    pygame.draw.line(WIN, (0, 0, 0), (col * SQUARE_SIZE + 10, row * SQUARE_SIZE + 10), (col * SQUARE_SIZE + SQUARE_SIZE - 10, row * SQUARE_SIZE + SQUARE_SIZE - 10), 5)
-                    pygame.draw.line(WIN, (0, 0, 0), (col * SQUARE_SIZE + SQUARE_SIZE - 10, row * SQUARE_SIZE + 10), (col * SQUARE_SIZE + 10, row * SQUARE_SIZE + SQUARE_SIZE - 10), 5)
+from visual import draw_grid, draw_popup
+from logic import game_condition
+from globals import SQUARE_SIZE, board
 
 
-def game_condition():
-    # Check rows
-    for row in range(ROWS):
-        if board[row][0] != -1 and board[row][0] == board[row][1] == board[row][2]:
-            return board[row][0]
-    
-    # Check columns
-    for col in range(COLS):
-        if board[0][col] != -1 and board[0][col] == board[1][col] == board[2][col]:
-            return board[0][col]
-    
-    # Check main diagonal
-    if board[0][0] != -1 and board[0][0] == board[1][1] == board[2][2]:
-        return board[0][0]
-    
-    # Check secondary diagonal
-    if board[0][2] != -1 and board[0][2] == board[1][1] == board[2][0]:
-        return board[0][2]
-    
-    # Check for tie
-    count = sum(row.count(-1) for row in board)
-    if count == 0:
-        return -2
-    
-    return -1
-
-def draw_popup(text):
-    # Create a new surface (i.e., a popup)
-    popup = pygame.Surface((200, 100))  # Adjust the size as needed
-    popup.fill((200, 200, 200))  # Adjust the color as needed
-
-    # Create a font object
-    font = pygame.font.Font(None, 36)  # Adjust the size as needed
-
-    # Render the text
-    text_surface = font.render(text, True, (0, 0, 0))  # Adjust the color as needed
-
-    # Get the width and height of the text surface
-    text_width, text_height = text_surface.get_size()
-
-    # Draw the text onto the popup surface
-    popup.blit(text_surface, ((200 - text_width) // 2, (100 - text_height) // 2))  # Adjust the size as needed
-
-    # Draw the popup onto the main surface
-    WIN.blit(popup, ((WIDTH - 200) // 2, (HEIGHT - 100) // 2))  # Adjust the size as needed
         
 def main():
+    pygame.init()
+    pygame.display.set_caption("Tic Tac Toe")
+
     run = True
     current_player = 1
+    
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
